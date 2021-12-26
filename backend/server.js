4f6b83e1527b868529a5fef23dd3fe6b3f4e2a85
Micah Wibowo <wibowo@calpoly.edu>
2021-12-25 19:41:59 -0800

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const bodyParser = require('body-parser')
+const cors = require("cors");
+
+const app = express() 
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Hi from Woods Humane Society!')
+})
+
+app.listen(3001) 
