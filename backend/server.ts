7c084b6d3cddf025cbb1f7400ba26866a929fd4d
Micah Wibowo <wibowo@calpoly.edu>
2022-01-09 12:10:16 -0800

    added ESLint and updated package.json files

--- backend/server.ts
+++ backend/server.ts
@@ -1,14 +1,14 @@
-const express = require("express")
-const bodyParser = require('body-parser')
-const cors = require("cors");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-const app = express() 
+const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.send('Hi from Woods Humane Society!')
-})
+  res.send('Hi from Woods Humane Society!');
+});
 
-app.listen(3001) 
+app.listen(3001);
