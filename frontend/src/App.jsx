53c425d659221c6c32772efbe9b56f72bf0a0c00
Ej1seven <erik.jamison.hunter@gmail.com>
2022-01-09 23:15:58 -0600

    added pages folder and login subfolder. I also added react-router-dom to navigate between pages. The Login component has been created. I will now begin styling the login page.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,20 @@
 /* eslint-disable react/jsx-filename-extension */
-import React from 'react';
-import logo from './imgs/logo.svg';
-import './css/App.css';
+import React from "react";
+import logo from "./imgs/logo.svg";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Login from "./pages/login/login";
+
+import "./css/App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.jsx</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ frontend/src/pages/login/login.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./login.css";
+
+const Login = () => (
+  <div>
+    <p>Login Page</p>
+  </div>
+);
+
+export default Login;
