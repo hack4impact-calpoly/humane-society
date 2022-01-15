63c8e4dc124150e3811f09b1bcea808deeaa1581
jonathanalaksana <jonathanalaksana@gmail.com>
2022-01-14 19:07:52 -0800

    Add react router and centered login form on page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,19 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import './css/App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './login';
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<p>Sign up</p>} />
+          <Route path="/forgotpassword" element={<p>forgot password</p>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- frontend/src/login.jsx
+++ frontend/src/login.jsx
@@ -1,35 +1,79 @@
 import React from 'react';
 import './css/login.css';
 import { Button, Grid, TextField } from '@mui/material';
+import { Link } from 'react-router-dom';
 import logo from './imgs/logo.png';
 import logoSmall from './imgs/logo-small.png';
 
 export default function Login() {
   return (
-    <div>
-      <Grid container direction="row">
-        <Grid item xs={12} sm={6}>
-          <Grid container direction="column">
+    <div className="loginPage">
+      <Grid
+        container
+        direction="row"
+        style={{ height: '1000px' }}
+      >
+        <Grid
+          item
+          sm={12}
+          md={6}
+          container
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+          spacing={2}
+        >
+          <Grid item>
             <img id="loginLogo" src={logo} alt="logo" />
-            <TextField
-              id="loginEmail"
-              label="Email"
-              placeholder="Enter your email"
-            />
-            <TextField
-              id="loginPassword"
-              label="Password"
-              type="password"
-              placeholder="Password"
-            />
-            <p>Forgot Password?</p>
+          </Grid>
+          <Grid
+            item
+            container
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+            spacing={3}
+          >
+            <Grid item>
+              <TextField
+                id="loginEmail"
+                label="Email"
+                placeholder="Enter your email"
+              />
+            </Grid>
+            <Grid item>
+              <TextField
+                id="loginPassword"
+                label="Password"
+                type="password"
+                placeholder="Password"
+              />
+              <br />
+              <Link to="/forgotpassword" style={{ float: 'right' }}>Forgot Password?</Link>
+            </Grid>
+          </Grid>
+          <Grid item>
             <Button sx={{ color: '#4aa7ac' }}>Login</Button>
-            <p>Don&apos;t have an account? Create an account here.</p>
+          </Grid>
+          <Grid item>
+            Don&apos;t have an account?&nbsp;
+            <Link to="/signup">Create an account here.</Link>
           </Grid>
         </Grid>
-        {/* TODO make second half disappear in xs screens (for mobile) and extend to end of page */}
-        <Grid item xs={0} sm={6} style={{ background: '#1d4d71' }}>
-          <img id="loginLogoSmall" src={logoSmall} alt="logo" />
+        {/* TODO make second half extend to end of page */}
+        <Grid
+          item
+          md={6}
+          container
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+          style={{ background: '#1d4d71' }}
+          sx={{ display: { xs: 'none', sm: 'none', md: 'block' } }}
+        >
+          <Grid item>
+            <img id="loginLogoSmall" src={logoSmall} alt="logo" />
+          </Grid>
         </Grid>
       </Grid>
     </div>
