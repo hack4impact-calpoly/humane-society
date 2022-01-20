d0443958adbdeefe08308debb4beec1df642caf2
Colem18 <cole2018@gmail.com>
2022-01-19 20:03:32 -0800

    getAllUsers and getUserById endpoints written

--- backend/server.js
+++ backend/server.js
@@ -8,13 +8,26 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var signup = require('./signup')
-var login = require('./login')
+var login = require('./login');
+const User = require('./models/user');
+const { resolveSoa } = require('dns');
 
 app.use('/signup', signup)
 app.use('/login', login)
 
 app.get('/', (req, res) => {
   res.send('Hi from Woods Humane Society!');
+});
+
+app.get('/getAllUsers', async (req, res) =>{
+  const allUsers = await User.find({});
+  res.send(allUsers);
+});
+
+app.get('/getUserById/:userId', async (req, res) => {
+  const userId = req.params.userId;
+  const user = await User.find({userID: userId});
+  res.send(user);
 });
 
 app.listen(3001);
