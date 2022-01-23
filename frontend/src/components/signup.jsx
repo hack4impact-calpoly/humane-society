828f80098e3a5babedac89c85da6ee4f7f8f0fe5
Micah Wibowo <wibowo@calpoly.edu>
2022-01-22 22:33:23 -0800

    updated signup page

--- frontend/src/components/signup.jsx
+++ frontend/src/components/signup.jsx
@@ -1,103 +1,142 @@
-/* eslint-disable */
 import React, { useState } from 'react';
-import '../css/signup.css'
-//testing to see if commits work
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import '../css/login.css';
 
-export default function signupPage() {
-  // States for registration
-  const [name, setName] = useState('');
+export default function SignUp() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
-  const [number, setNumber] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
   const [password, setPassword] = useState('');
-  // States for checking the errors
-  const [submitted, setSubmitted] = useState(false);
-  const [error, setError] = useState(false);
-  // Handling the name change
-  const handleName = (e) => {
-    setName(e.target.value);
-    setSubmitted(false);
+
+  const verifySignup = () => {
+    // const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    // if (!email.match(validRegex)) {
+    //   alert('Please use a valid email');
+    // } else if (!phoneNumber.match(/\d/g).length === 10) {
+    //   alert('Please use a valid phone number');
+    // } else if (password.length < 8) {
+    //   alert('Please use a password that is longer than 8 characters');
+    // } else {
+    console.log(firstName, lastName, email, phoneNumber, password);
+    // function call to datdabase here
+    // }
   };
-  // Handling the email change
-  const handleEmail = (e) => {
-    setEmail(e.target.value);
-    setSubmitted(false);
-  };
-  // Handling the number change
-  const handleNumber = (e) => {
-    setNumber(e.target.value);
-    setSubmitted(false);
-  };
-  // Handling the password change
-  const handlePassword = (e) => {
-    setPassword(e.target.value);
-    setSubmitted(false);
-  };
-  // Handling the form submission
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (name === '' || email === '' || password === '') {
-      setError(true);
-    } else {
-      setSubmitted(true);
-      setError(false);
-    }
-  };
-  // Showing success message
-  const successMessage = () => {
-    return (
-      <div
-        className="success"
-        style={{
-          display: submitted ? '' : 'none',
-        }}>
-        <h1>
-          This User,
-           {name}
-           successfully registered!!
-        </h1>
-      </div>
-    );
-  };
-  // Showing error message if error is true
-  const errorMessage = () => {
-    return (
-      <div
-        className="error"
-        style={{
-          display: error ? '' : 'none',
-        }}>
-        <h1>Please enter all the fields</h1>
-      </div>
-    );
-  };
+
   return (
-    <div className="form">
-      <div>
-        <div className="account_header">Create an account</div>
-      </div>
-      {/* Calling to the methods */}
-      <div className="messages">
-        {errorMessage()}
-        {successMessage()}
-      </div>
-      <form>
-        {/* Labels and inputs for form data */}
-        <label className="label">Name</label>
-          <input onChange={handleName} className="input"
-          value={name} type="text" />
-        <label className="label">Email</label>
-        <input onChange={handleEmail} className="input"
-          value={email} type="email" />
-        <label className="label">Phone Number</label>
-        <input onChange={handleNumber} className="input"
-          value={number} type="number" />
-        <label className="label">Password</label>
-        <input onChange={handlePassword} className="input"
-          value={password} type="password" />
-        <button onClick={handleSubmit} className="btn" type="submit">
-          Create account
-        </button>
-      </form>
+    <div className="loginPage">
+      <Container component="main" maxWidth="xs">
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <Typography component="h1" variant="h5">
+            Create an account
+          </Typography>
+          <Box component="form" noValidate onSubmit={verifySignup} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="given-name"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  value={firstName}
+                  onChange={(e) => { setFirstName(e.target.value); }}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="family-name"
+                  value={lastName}
+                  onChange={(e) => { setLastName(e.target.value); }}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  value={email}
+                  onChange={(e) => { setEmail(e.target.value); }}
+                  error={!email.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/) && !email.length === 0}
+                  helperText={!email.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/) && !email.length === 0 ? 'Must use valid email' : ''}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="phoneNumber"
+                  label="Phone Number"
+                  id="phoneNumber"
+                  autoComplete="phoneNumber"
+                  value={phoneNumber}
+                  onChange={(e) => { setPhoneNumber(e.target.value); }}
+                  error={!phoneNumber.length === 0}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                  value={password}
+                  onChange={(e) => { setPassword(e.target.value); }}
+                  error={password.length < 8 && !password.length === 0}
+                  helperText={password.length < 8 && !password.length === 0 ? 'Password must be atleast 8 characters' : ''}
+                />
+              </Grid>
+            </Grid>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="/" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              style={{
+                borderRadius: 8,
+                backgroundColor: '#21b6ae',
+
+              }}
+            >
+              Create account
+            </Button>
+          </Box>
+        </Box>
+      </Container>
     </div>
   );
 }
+//                  error={password.length > 8 && !password.length === 0}
+//                  error={!phoneNumber.match(/\d/g).length === 10 && !phoneNumber.length === 0}
