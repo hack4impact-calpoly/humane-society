32c4a0fcacb5cf14749f37671b227c94d25f2d32
noximus <SageM5862@gmail.com>
2022-01-24 13:14:51 -0800

    availibility schema

---
+++ backend/models/availability.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose")
+const { userConnection } = require('../backend');
+
+const AvailabilitySchema = new mongoose.Schema({
+    times: [{ start: { type: Date, required: false }, end: { type: Date, required: false } }],
+    volunteer: {
+        type: {
+            userID: { type: String, required: true },
+            firstName: { type: String, required: true },
+            lastName: { type: String, required: true },
+            userName: { type: String, required: true },
+            email: { type: String, required: true },
+        },
+        required: true,
+    },
+    usingDefaultTimes: { type: Boolean, required: true },
+    completed: { type: Boolean, required: false, default: false },
+    completedStatusSet: { type: Boolean, required: false, default: false },
+})
+
+const Availability = userConnection.model('availability', UserSchema);
+module.exports = Availability
