327c86261ede43b667ad02a12ea3dc24c0144693
Micah Wibowo <wibowo@calpoly.edu>
2022-01-31 21:16:00 -0800

    css

--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -2,9 +2,6 @@ import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
   palette: {
-    primary: {
-      main: '#ffffff',
-    },
     secondary: {
       main: '#4aa7ac',
       contrastText: '#ffffff',
