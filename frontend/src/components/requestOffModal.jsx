cf5d35c53e12613858df901783841384ee57b723
jonathanalaksana <jonathanalaksana@gmail.com>
2022-02-13 16:39:42 -0800

    create textfield and modal grid layout

--- frontend/src/components/requestOffModal.jsx
+++ frontend/src/components/requestOffModal.jsx
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
-import { Button, TextField } from '@mui/material/';
+import { Button, TextField, Grid } from '@mui/material/';
 import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
+import '../css/requestOffModal.css';
 
 export default function RequestOffModal() {
   const [open, setOpen] = useState(false);
   // const [startDate, setStartDate] = useState(''); // set default to today
   // const [endDate, setendDate] = useState('');
-  // const [notes, setNotes] = useState('');
+  const [notes, setNotes] = useState('');
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -36,45 +36,67 @@ export default function RequestOffModal() {
       <Dialog open={open} onClose={handleClose}>
         <DialogTitle><b>Date(s):</b></DialogTitle>
         <DialogContent>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="notes"
-            label="Email Address"
-            type="email"
-            fullWidth
-            variant="standard"
-          />
+          <Grid
+            container
+            className="body"
+            direction="column"
+            justifyContent="space-between"
+          >
+            <Grid
+              item
+              container
+              justifyContent="space-between"
+            >
+              <p>date1</p>
+              <p>to</p>
+              <p>date2</p>
+            </Grid>
+            <Grid item>
+              <TextField
+                id="notes"
+                label="Notes"
+                multiline
+                fullWidth
+                rows={6}
+                value={notes}
+                onChange={(e) => { setNotes(e.target.value); }}
+              />
+            </Grid>
+            <Grid
+              item
+              container
+              justifyContent="space-between"
+            >
+              <Button
+                variant="outlined"
+                size="small"
+                onClick={handleClose}
+                sx={{
+                  width: 100,
+                  color: 'black',
+                  border: '2px black solid',
+                }}
+                style={{
+                  borderRadius: 0,
+                }}
+              >
+                Cancel
+              </Button>
+              <Button
+                variant="contained"
+                color="secondary"
+                size="small"
+                onClick={submitRequest}
+                style={{
+                  borderRadius: 0,
+                  minWidth: '100px',
+                }}
+              >
+                Request
+              </Button>
+            </Grid>
+          </Grid>
         </DialogContent>
-        <DialogActions>
-          <Button
-            variant="outlined"
-            size="small"
-            onClick={handleClose}
-            sx={{
-              width: 100,
-              color: 'black',
-              border: '1px black solid',
-            }}
-            style={{
-              borderRadius: 0,
-            }}
-          >
-            Cancel
-          </Button>
-          <Button
-            variant="contained"
-            color="secondary"
-            size="small"
-            onClick={submitRequest}
-            style={{
-              borderRadius: 0,
-              minWidth: '100px',
-            }}
-          >
-            Request
-          </Button>
-        </DialogActions>
       </Dialog>
     </div>
   );
