03ebda4537a6c1e1cc5f1a15638630bc24438da1
jonathanalaksana <jonathanalaksana@gmail.com>
2022-02-27 15:23:15 -0800

    Add calendar with views and test appts

---
+++ frontend/src/components/availability/appointments.js
@@ -0,0 +1,60 @@
+const appointments = [
+  {
+    title: '',
+    startDate: new Date(2022, 1, 27, 12, 11),
+    endDate: new Date(2022, 1, 27, 13, 0),
+    id: 1,
+  }, {
+    title: '',
+    startDate: new Date(2022, 1, 28, 14, 30),
+    endDate: new Date(2022, 1, 28, 15, 35),
+    id: 2,
+  }, {
+    title: '',
+    startDate: new Date(2022, 2, 1, 10, 0),
+    endDate: new Date(2022, 2, 1, 15, 0),
+    id: 3,
+  }, {
+    title: '',
+    startDate: new Date(2022, 2, 2, 14, 30),
+    endDate: new Date(2022, 2, 2, 16, 45),
+    id: 5,
+  }, {
+    title: '',
+    startDate: new Date(2022, 2, 3, 9, 45),
+    endDate: new Date(2022, 2, 3, 11, 15),
+    id: 6,
+  }, {
+    title: '',
+    startDate: new Date(2022, 2, 4, 12, 0),
+    endDate: new Date(2022, 2, 4, 14, 0),
+    id: 7,
+  }, {
+    title: '',
+    startDate: new Date(2022, 2, 5, 15, 15),
+    endDate: new Date(2022, 2, 5, 16, 30),
+    id: 8,
+  }, {
+    title: '',
+    startDate: new Date(2022, 2, 6, 11, 0),
+    endDate: new Date(2022, 2, 6, 15, 0),
+    id: 9,
+  }, {
+    title: '',
+    startDate: new Date(2022, 2, 7, 11, 0),
+    endDate: new Date(2022, 2, 7, 13, 30),
+    id: 10,
+  }, {
+    title: '',
+    startDate: new Date(2022, 2, 8, 14, 0),
+    endDate: new Date(2022, 2, 8, 15, 30),
+    id: 11,
+  }, {
+    title: '',
+    startDate: new Date(2022, 2, 9, 10, 0),
+    endDate: new Date(2022, 2, 9, 11, 30),
+    id: 12,
+  },
+];
+
+export default appointments;
--- frontend/src/components/availability/indicateAvailability.jsx
+++ frontend/src/components/availability/indicateAvailability.jsx
@@ -1,7 +1,42 @@
-import React from 'react';
+/* eslint-disable react/jsx-props-no-spreading */
+import { React } from 'react';
 import { Button } from '@mui/material';
+import { ViewState } from '@devexpress/dx-react-scheduler';
+import {
+  Scheduler,
+  WeekView,
+  MonthView,
+  Appointments,
+  AppointmentTooltip,
+  AppointmentForm,
+  Toolbar,
+  ViewSwitcher,
+  DateNavigator,
+  TodayButton,
+} from '@devexpress/dx-react-scheduler-material-ui';
+import appointments from './appointments';
+import '../../css/availability.css';
+
+function Appointment({ style, children, ...restProps }) {
+  return (
+    <Appointments.Appointment
+      {...restProps}
+      style={{
+        ...style,
+        backgroundColor: '#4aa7ac',
+        borderRadius: '5px',
+      }}
+    >
+      {children}
+    </Appointments.Appointment>
+  );
+}
 
 export default function IndicateAvailability() {
+  // Have calendar default on current date
+  const curDate = new Date();
+  const defaultDate = `${curDate.getFullYear()}-${String(curDate.getMonth() + 1).padStart(2, '0')}-${String(curDate.getDate()).padStart(2, '0')}`;
+
   return (
     <div>
       <div style={{ textAlign: 'right' }}>
@@ -11,6 +46,36 @@ export default function IndicateAvailability() {
         >
           + Add Availabilty
         </Button>
+      </div>
+      <div style={{ padding: '5%', paddingLeft: '0px' }}>
+        <Scheduler data={appointments} height={660}>
+          <ViewState
+            defaultCurrentViewName="Week"
+            defaultCurrentDate={defaultDate}
+          />
+          <WeekView
+            startDayHour={8}
+            endDayHour={17}
+            cellDuration={60}
+          />
+          <MonthView
+            intervalCount={0.8}
+          />
+          <Toolbar />
+          <DateNavigator />
+          <TodayButton />
+          <ViewSwitcher />
+          <Appointments
+            appointmentComponent={Appointment}
+          />
+          <AppointmentTooltip
+            showCloseButton
+            showOpenButton
+          />
+          <AppointmentForm
+            readOnly
+          />
+        </Scheduler>
       </div>
     </div>
   );
