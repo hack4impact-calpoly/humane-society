133a6e47ad4aa5589a85c920aa62140fc5d5a9dc
Micah Wibowo <wibowo@calpoly.edu>
2022-03-01 18:51:14 -0800

    formatting

--- backend/src/updateProfile.js
+++ backend/src/updateProfile.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { emit } = require('process');
+
 const { Auth } = 'aws-amplify';
 
 const router = express.Router();
@@ -8,29 +9,33 @@ require('dotenv').config();
 const User = require('../models/user');
 
 router.post('/', async (req, res) => {
-    const { userID, phone, firstName, lastName, email }  = req.body;
+  const {
+    userID, phone, firstName, lastName, email,
+  } = req.body;
 
-    const user = User;
-    console.log('Updating profile...');
-    console.log(userID)
-    console.log(firstName)
-    user.findOneAndUpdate({ userID }, {
-        phone: phone, firstName: firstName,
-        lastName: lastName, email: email
+  const user = User;
+  console.log('Updating profile...');
+  console.log(userID);
+  console.log(firstName);
+  user.findOneAndUpdate({ userID }, {
+    phone,
+    firstName,
+    lastName,
+    email,
+  })
+    .then(() => {
+      console.log('update successful');
+      res.status(200).send('success');
     })
-        .then(() => {
-            console.log('update successful');
-            res.status(200).send('success');
-        })
-        .catch((err) => {
-            console.log(err);
-            res.status(500).send('error');
-        });
-    /* update other attributes */
-    /* update email in aws cognito + email verification */
-    // {bypassCache: true}
-    // NOTE: Come back once user auth is fully integrated
-   /* const awsUser = await Auth.currentAuthenticatedUser().then(() => {
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send('error');
+    });
+  /* update other attributes */
+  /* update email in aws cognito + email verification */
+  // {bypassCache: true}
+  // NOTE: Come back once user auth is fully integrated
+  /* const awsUser = await Auth.currentAuthenticatedUser().then(() => {
         await Auth.updateUserAttributes(awsUser, {
             'phone': phone, 'firstName': firstName, 'lastName': lastName
         })
@@ -41,8 +46,7 @@ router.post('/', async (req, res) => {
             console.log(err);
             res.status(500).send('error');
         });
-    */    
-
+    */
 });
 
 module.exports = router;
