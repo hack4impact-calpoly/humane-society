64e96caf9001d4b5cfda5f69bad17261c7cc6c53
noximus <SageM5862@gmail.com>
2022-03-30 12:17:03 -0700

    delete availability done

--- backend/src/UpdateAvailability.js
+++ backend/src/UpdateAvailability.js
@@ -6,13 +6,13 @@ require('dotenv').config();
 const Availability = require('../models/availability');
 
 router.post('/newAvailability', async (req, res) => {
-  const { userID, times, startDate, endDate, reoccurrence } = req.body;
+  const { userID, times, startDate, endDate, reoccurrence, recDay } = req.body;
   const availability = Availability;
   console.log('inside function');
 
   let doc;
   doc = new availability({
-    userID, times, startDate, endDate, reoccurrence,
+    userID, times, startDate, endDate, reoccurrence, recDay,
   });
 
   doc.save();
@@ -21,11 +21,11 @@ router.post('/newAvailability', async (req, res) => {
 });
 
 router.post('/updateAvailability', async (req, res) => {
-  const { _id, times } = req.body;
+  const { _id, times, reoccurrence, recDay } = req.body;
   const availability = Availability;
   console.log(times);
 
-  availability.updateOne({ _id }, { times }).then((result) => {
+    availability.updateOne({ _id }, { times }, { reoccurrence }, {recDay}).then((result) => {
     if (result) {
       res.status(200).send('updated successfully');
     }
@@ -33,6 +33,22 @@ router.post('/updateAvailability', async (req, res) => {
     console.log(err);
     res.status(500).send('could not update');
   });
+});
+
+/* deletes a availibility document based on userID */
+router.post('/deleteAvailability', async (req, res) => {
+    const { _id } = req.body;
+    const availability = Availability;
+    var mongodb = require('mongodb');
+
+    availability.deleteOne({ _id: new mongodb.ObjectID(_id) }).then((result) => {
+        if (result) {
+            res.status(200).send('deleted successfully');
+        }
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).send('could not delete');
+    });
 });
 
 module.exports = router;
