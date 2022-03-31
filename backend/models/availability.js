5bf762a1bb89eb088ccdf730d005a18cb49c3599
Micah Wibowo <wibowo@calpoly.edu>
2022-03-31 10:17:48 -0700

    formatting

--- backend/models/availability.js
+++ backend/models/availability.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 const { userConnection } = require('../connection');
 
 const AvailabilitySchema = new mongoose.Schema({
-    userID: { type: String, required: true },
-    times: [{ startTime: { type: Date, required: false }, endTime: { type: Date, required: false } }],
-    startDate: { type: Date, required: false },
-    endDate: { type: Date, required: false },
-    reoccurrence: { type: Boolean, required: true, default: false },
-    recDay:  [{ type: Number, min: 0, max: 6 }],
+  userID: { type: String, required: true },
+  times: [{ startTime: { type: Date, required: false }, endTime: { type: Date, required: false } }],
+  startDate: { type: Date, required: false },
+  endDate: { type: Date, required: false },
+  reoccurrence: { type: Boolean, required: true, default: false },
+  recDay: [{ type: Number, min: 0, max: 6 }],
 });
 
 const Availability = userConnection.model('availability', AvailabilitySchema);
