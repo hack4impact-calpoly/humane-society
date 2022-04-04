6e7e7abc7fc2dfecd6fc30f365f274ba54be1ab9
sleepyStick <itsirisho@gmail.com>
2022-04-03 21:30:15 -0700

    added success page

---
+++ frontend/src/components/signupSuccess.jsx
@@ -0,0 +1,47 @@
+/* eslint-disable no-restricted-globals */
+/* eslint-disable no-useless-escape */
+import React from 'react';
+import {
+  Button, Grid, Box, Typography, Container,
+} from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import logo from '../imgs/signupLogo.svg';
+
+export default function SignupSuccess() {
+  const navigate = useNavigate();
+  return (
+    <div className="signupSuccessPage">
+      <Container component="main" maxWidth="xs">
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <Grid item>
+            <img id="signupLogo" src={logo} alt="logo" />
+          </Grid>
+          <Typography component="h1" variant="h5">
+            Account Successfully Created
+          </Typography>
+          <Box component="form" noValidate sx={{ width: 300 }}>
+            <Button
+              onClick={() => navigate('/login')}
+              fullWidth
+              variant="contained"
+              color="secondary"
+              sx={{ mt: 3, mb: 2 }}
+              style={{
+                borderRadius: 8,
+              }}
+            >
+              Return to login
+            </Button>
+          </Box>
+        </Box>
+      </Container>
+    </div>
+  );
+}
