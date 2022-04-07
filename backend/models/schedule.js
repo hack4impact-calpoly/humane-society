79b4a7892f74200c3091ad95067768752075df07
Micah Wibowo <wibowo@calpoly.edu>
2022-04-06 17:58:09 -0700

    formatting

--- backend/models/schedule.js
+++ backend/models/schedule.js
@@ -2,20 +2,20 @@ const mongoose = require('mongoose');
 const { userConnection } = require('../connection');
 
 const SchedulingSchema = new mongoose.Schema({
-    scheduleID: { type: String, required: true },
-    userID: { type: String, required: true },
-    Date: { type: Date, required: true },
-    startTime: { type: Date, required: true },
-    endTime: { type: Date, required: true },
-    Tasks: [{ type: String, required: true }],
+  scheduleID: { type: String, required: true },
+  userID: { type: String, required: true },
+  Date: { type: Date, required: true },
+  startTime: { type: Date, required: true },
+  endTime: { type: Date, required: true },
+  Tasks: [{ type: String, required: true }],
 });
 
 SchedulingSchema.pre('validate', function (next) {
-    if (this.startTime > this.endTime) {
-        next(new Error('End Time must be greater than Start Time'));
-    } else {
-        next();
-    }
+  if (this.startTime > this.endTime) {
+    next(new Error('End Time must be greater than Start Time'));
+  } else {
+    next();
+  }
 });
 
 const Scheduling = userConnection.model('schedule', SchedulingSchema);
