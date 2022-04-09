edb697a46c3d4ea81439992661c2c71758961739
noximus <SageM5862@gmail.com>
2022-04-09 12:15:28 -0700

    add taskbar

--- frontend/src/components/contacts.jsx
+++ frontend/src/components/contacts.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable */
 import { DataGrid } from '@mui/x-data-grid';
 import React, { useState, useEffect } from 'react';
+import Taskbar from "./taskbar"
 import '../css/contacts.css';
 
 const columns = [
@@ -9,7 +10,7 @@ const columns = [
     { field: "phoneNumber", headerName: "phone number", flex: 1 }
 ];
 const rows = [
-    { id: 0,name: 0, email: "Task 1", phoneNumber: 20 },
+    { id: 0, name: 0, email: "Task 1", phoneNumber: 20 },
     { id: 1, name: 1, email: "Task 2", phoneNumber: 40 },
     { id: 2, name: 2, email: "Task 3", phoneNumber: 60 }
 ];
@@ -19,26 +20,25 @@ const rows = [
 export default function Availability() {
     const [users, setUsers] = useState([]);
 
+    const formatUsers = () => {
+
+    }
     const getUsers = async () => {
-       const response = await fetch('http://localhost:3001/getUsers/getAllUsers', {
+        const response = await fetch('http://localhost:3001/getUsers/getAllUsers', {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
             },
-        }).then((result) => {
-            console.log(result);
-            if (result.status === 200) {
-                // success
-            
-            } else {
-                console.log('error');
-            }
-        });
-        data = await response.json()
-        console.log(data)
+        })
+        if (response.status === 200) {
+            const data = await response.json()
+            setUsers(data)
+        } else {
+            console.log("could not get users")
+        }
     }
-    
-    
+
+
     useEffect(() => {
         getUsers()
         console.log(users)
@@ -46,16 +46,26 @@ export default function Availability() {
 
 
     return (
+        <div>
+        <Taskbar/>
         <div className="contacts">
             <DataGrid
                 autoHeight
                 columns={columns}
                 rows={rows}
+ 
                 pageSize={5}
                 autoPageSize
-                
-                
             />
-        </div>
+        </div></div>
     );
 }
+
+
+
+
+
+
+
+
+
