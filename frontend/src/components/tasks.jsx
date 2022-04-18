6cd63b88a4290686c6230d173aa047fb0091a076
jonathanalaksana <jonathanalaksana@gmail.com>
2022-04-17 18:34:16 -0700

    mini calendar

--- frontend/src/components/tasks.jsx
+++ frontend/src/components/tasks.jsx
@@ -1,10 +1,19 @@
-import { React } from 'react';
+import { React, useState } from 'react';
+import DatePicker from 'sassy-datepicker';
 import CircularProgressWithLabel from './circularProgress';
 
 export default function Task() {
+  const [date, setDate] = useState(new Date());
+
+  const onChange = (newDate) => {
+    setDate(newDate);
+  };
+
   return (
     <div>
       <CircularProgressWithLabel value={50} />
+      <DatePicker onChange={onChange} selected={date} />
+      <p>{date.toString()}</p>
     </div>
   );
 }
