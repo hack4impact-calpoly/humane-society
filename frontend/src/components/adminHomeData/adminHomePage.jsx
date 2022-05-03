628684d9f89ea7bae9e16f642c4de9df4554a574
sbalijep <sameerabali5@gmail.com>
2022-05-02 21:30:25 -0700

    trying to incorporate calendars

--- frontend/src/components/adminHomeData/adminHomePage.jsx
+++ frontend/src/components/adminHomeData/adminHomePage.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import { React, useState } from 'react';
 import AdminCalendar from './adminCalendar';
 import AdminHomeTaskbar from './adminHomeTaskbar';
 import AdminMainCalendar from './adminMainCalendar';
 
+
 export default function RequestOff() {
+  const [date, setDate] = useState(new Date());
+  const onChange = (newDate) => {
+    setDate(newDate);
+  };
   return (
     <div>
+      <DatePicker onChange={onChange} selected={date} />
       <AdminHomeTaskbar />
       <AdminCalendar />
       <AdminMainCalendar />
