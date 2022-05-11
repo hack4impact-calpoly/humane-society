bd591f5d329c5bd48656fbef627ea683e0e10573
Micah Wibowo <wibowo@calpoly.edu>
2022-05-11 10:18:59 -0700

    changed taskbar updated some other stuff

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,51 +15,13 @@ import Profile from './components/profile';
 import Request from './components/requestOff';
 import Availability from './components/availability';
 import Contacts from './components/contacts';
-<<<<<<< HEAD
-import Availability from './components/availability/availability';
-import RequestOffAdmin from './components/requestOffAdmin';
-import AdminHomePage from './components/adminHomeData/adminHomePage';
-
-function App() {
-  return (
-    <div className="App">
-      <ThemeProvider theme={theme}>
-        <BrowserRouter>
-          <Routes>
-            { /* Public Routes */ }
-            <Route path="/" element={<p>landing page</p>} />
-            <Route path="/login" element={<Login />} exact />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/signup/success" element={<SignupSuccess />} />
-            <Route path="/forgotpassword" element={<ForgotPassword />} />
-            <Route path="/passwordreset" element={<PasswordReset />} />
-            <Route path="/resetpassword" element={<ResetPassword />} />
-            { /* Private Routes */ }
-            <Route element={<RequireAuth />}>
-              <Route path="/contacts" element={<Contacts />} />
-              <Route path="/tasks" />
-              <Route path="/request-off-admin" element={<RequestOffAdmin />} />
-              <Route path="/adminhomepage" element={<AdminHomePage />} />
-              <Route path="/request-off" element={<Request />} />
-              <Route path="/availability" element={<Availability />} />
-              <Route path="/availability" element={<Availability />} />
-              <Route path="/discussions" />
-              <Route path="profile" element={<Profile />} />
-            </Route>
-            { /* catch all route */ }
-            <Route path="*" element={<h1>404 page not found</h1>} />
-          </Routes>
-        </BrowserRouter>
-      </ThemeProvider>
-    </div>
-  );
-=======
 import { Fragment } from 'react';
 import Task from './components/tasks';
 import RequestAdmin from './components/requestOffAdmin';
+import AdminHomePage from './components/adminHomeData/adminHomePage';
 
 /* helps to only allow admins and employees to see certain pages */
-function isAdmin(props) {
+function isAdmin() {
     const isAdmin = localStorage.getItem('isAdmin');
 
     if (isAdmin == "true") {
@@ -88,7 +50,7 @@ function App() {
                             {isAdmin() ?
                                 (<Fragment> <Route path="/contacts" element={<Contacts />} />
                                      <Route path="/request-off-admin" element={<RequestAdmin />} />
-                                     <Route path="/adminhomepage" />
+                                     <Route path="/adminhomepage" element={<AdminHomePage />} />
                                      <Route path="/admin-tasks" />
                                 </Fragment>)
                                 :
@@ -106,6 +68,5 @@ function App() {
             </ThemeProvider>
         </div>
     );
->>>>>>> 1e0711d89dbecc56d32258ca8d4d0150d2b8c78b
 }
 export default App;
--- frontend/src/components/adminHomeData/adminHomePage.jsx
+++ frontend/src/components/adminHomeData/adminHomePage.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import AdminHomeTaskbar from './adminHomeTaskbar';
 import AdminCalendar from './adminCalendar';
+import AdminTaskbar from '../TaskBar/adminTaskbar';
 
 export default function AdminHomePage() {
   return (
     <div>
-      <AdminHomeTaskbar />
+      <AdminTaskbar />
       <AdminCalendar />
     </div>
   );
