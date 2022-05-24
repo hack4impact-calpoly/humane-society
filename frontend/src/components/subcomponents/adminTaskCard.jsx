c7a9c62d27412d9e6fd287588702317ae986c2ba
Micah Wibowo <wibowo@calpoly.edu>
2022-05-24 09:39:16 -0700

    css

--- frontend/src/components/subcomponents/adminTaskCard.jsx
+++ frontend/src/components/subcomponents/adminTaskCard.jsx
@@ -1,61 +1,60 @@
-/* eslint-disable */
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
 import {
-    Card, Checkbox, CardContent, IconButton, Collapse, Typography,
+  Card, Checkbox, CardContent, IconButton, Collapse, Typography,
 } from '@mui/material';
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ExpandLessIcon from '@mui/icons-material/ExpandLess';
-import '../../css/adminTaskCard.css';
+import '../../css/taskCard.css';
 
 function ExpandIcon({ open }) {
-    if (open === true) {
-        return <ExpandMoreIcon sx={{ color: 'black' }} />;
-    }
-    return <ExpandLessIcon sx={{ color: 'black' }} />;
+  if (open === true) {
+    return <ExpandMoreIcon sx={{ color: 'black' }} />;
+  }
+  return <ExpandLessIcon sx={{ color: 'black' }} />;
 }
 
-function AdminTaskCard({
-    taskID, checked = false, setChecked, name, description,
+function TaskCard({
+  taskID, name, description,
 }) {
-    const [expand, setExpand] = useState(false);
+  const [expand, setExpand] = useState(false);
 
-    return (
-        <div>
-            <Card>
-                <CardContent sx={{ flexDirection: 'row', display: 'flex', justifyContent: 'space-between' }}>
-                    <div id="name">
-                        <Typography
-                            sx={{
-                                display: 'flex',
-                                pt: '.6rem',
-                            }}
-                        >
-                            {name}
-                        </Typography>
-                    </div>
-                    <IconButton onClick={() => setExpand(!expand)}>
-                        <ExpandIcon open={expand} />
-                    </IconButton>
-                </CardContent>
-                <Collapse in={expand}>
-                    <CardContent>
-                        <p id="desc">{description}</p>
-                    </CardContent>
-                </Collapse>
-            </Card>
-            <div id="lowerBorder" />
-        </div>
-    );
+  return (
+    <div>
+      <Card>
+        <CardContent sx={{ flexDirection: 'row', display: 'flex', justifyContent: 'space-between' }}>
+          <Typography
+            sx={{
+              display: 'flex',
+              pt: '.6rem',
+            }}
+          >
+            {name}
+          </Typography>
+          <IconButton onClick={() => setExpand(!expand)}>
+            <ExpandIcon open={expand} />
+          </IconButton>
+        </CardContent>
+        <Collapse in={expand}>
+          <CardContent>
+            <p id="desc">{description}</p>
+          </CardContent>
+        </Collapse>
+      </Card>
+      <div id="lowerBorder" />
+    </div>
+  );
 }
 
 ExpandIcon.propTypes = {
-    open: PropTypes.bool.isRequired,
+  open: PropTypes.bool.isRequired,
 };
 
-AdminTaskCard.propTypes = {
-    name: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired
+TaskCard.propTypes = {
+  name: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
 };
 
-export default AdminTaskCard;
+export default TaskCard;
