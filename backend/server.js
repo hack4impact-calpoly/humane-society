e748bf53d185b40a236d77dbf99bfb5626c6d4ef
sleepyStick <itsirisho@gmail.com>
2022-05-25 10:36:00 -0700

    using requestOff endpoints

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,7 @@ const updateProfile = require('./src/updateProfile');
 const schedule = require('./src/Scheduling');
 const task = require('./src/UpdateTask');
 const updatePassword = require('./src/updatePw');
+const requestOff = require('./src/requestOff');
 
 app.use('/signup', signup);
 app.use('/login', login);
@@ -24,6 +25,7 @@ app.use('/updateProfile', updateProfile);
 app.use('/task', task);
 app.use('/schedule', schedule);
 app.use('/updatePassword', updatePassword);
+app.use('/requestOff', requestOff);
 
 app.get('/', (req, res) => {
   res.send('Hi from Woods Humane Society!');
